fd43501fc13723bb8b32ef2511d77979203268ac
ZZ3n <ndsrhkd@naver.com>
2020-01-13 21:58:38 +0900

    트랜지션, 슬라이드 시도(Ajax)

--- public/js.js
+++ public/js.js
@@ -1,25 +1,21 @@
 $(function () {
-	
-    
-	function resizeHandler(evt) {
-        let $fixed_on_top = $("#fixed_on_top");
-	    let padding_on_top = $fixed_on_top.css("height");
-
-	    $("body").css("padding-top", padding_on_top);
-		console.log("resize!" + window.innerWidth);
-		const $NnE_img = $(".NnE-img");
-		if (window.innerWidth >= 768)
-		{
-			console.log("big");
-			$NnE_img.css("width","100%");
-            
-		}
-		else {
-			console.log("small");
-			$NnE_img.css("width","initial");
-		}
-	}
-    resizeHandler(null);
+	resizeHandler(null);
+	paddingOnTop();
 	window.addEventListener("resize",resizeHandler);
+	document.body.addEventListener("transitionend",paddingOnTop);
 });
 
+function resizeHandler(evt) {
+	console.log("resize!" + window.innerWidth);
+	//paddingOnTop();
+	const wrapper = document.getElementById("wrapper");
+	const menu = document.getElementById("Menu");
+	menu.style.right = window.getComputedStyle(wrapper).marginRight;
+}
+
+function paddingOnTop() {
+	console.log("paddingOnTop");
+	const fixed_on_top = document.getElementById("fixed_on_top");
+	document.body.style.paddingTop = fixed_on_top.clientHeight + "px";
+
+}
