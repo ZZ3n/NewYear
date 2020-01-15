e2a50ef4da33411f71a9bc665715962f99b86f93
ZZ3n <ndsrhkd@naver.com>
2020-01-15 22:31:25 +0900

    popupMenu 생성, 그림자(?) 추가.(아직 이상함)

--- public/js.js
+++ public/js.js
@@ -5,16 +5,16 @@ $(function () {
 	document.body.addEventListener("transitionend",paddingOnTop);
 });
 
-function resizeHandler(evt) {
-	console.log("resize!" + window.innerWidth);
-	//paddingOnTop();
+//리사이즈시 발생하는 콜백 함수. js로딩시에도 발생함. body 트랜지션을 일으킴.
+function resizeHandler(/*event*/) {
 	const wrapper = document.getElementById("wrapper");
 	const menu = document.getElementById("Menu");
 	menu.style.right = window.getComputedStyle(wrapper).marginRight;
 }
 
+//body에 패딩을 줘서 메뉴바가 페이지를 가리지 않도록 함.
+//body 트랜지션 엔드의 콜백함수임.
 function paddingOnTop() {
-	console.log("paddingOnTop");
 	const fixed_on_top = document.getElementById("fixed_on_top");
 	document.body.style.paddingTop = fixed_on_top.clientHeight + "px";
 
