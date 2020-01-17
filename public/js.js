954d06e7454cfc692de2375e3a6604a3821c25b0
ZZ3n <ndsrhkd@naver.com>
2020-01-17 22:33:21 +0900

    오늘은 많이 해매었다.

--- public/js.js
+++ public/js.js
@@ -1,6 +1,7 @@
 $(function () {
 	resizeHandler(null);
 	paddingOnTop();
+	btnPosition();
 	window.addEventListener("resize",resizeHandler);
 	document.body.addEventListener("transitionend",paddingOnTop);
 });
@@ -10,6 +11,7 @@ function resizeHandler(/*event*/) {
 	const wrapper = document.getElementById("wrapper");
 	const menu = document.getElementById("Menu");
 	menu.style.right = window.getComputedStyle(wrapper).marginRight;
+	btnPosition();
 }
 
 //body에 패딩을 줘서 메뉴바가 페이지를 가리지 않도록 함.
@@ -18,4 +20,12 @@ function paddingOnTop() {
 	const fixed_on_top = document.getElementById("fixed_on_top");
 	document.body.style.paddingTop = fixed_on_top.clientHeight + "px";
 
+}
+
+function btnPosition() {
+	console.log("123");
+	const sec1 = document.getElementById("section1");
+	const height = window.getComputedStyle(sec1).height;
+	document.getElementById("prev").style.top = Number(height.replace("px",""))/2 + "px";
+	document.getElementById("next").style.top = Number(height.replace("px",""))/2 + "px";
 }
